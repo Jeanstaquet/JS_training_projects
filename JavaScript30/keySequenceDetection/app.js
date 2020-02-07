3492c57c8f92dd07943ac546056c834f8648a361
Jeanstaquet <jean.staquet8@gmail.com>
2020-02-07 21:39:59 +0100

    uptdate dossier

--- JavaScript30/keySequenceDetection/app.js
+++ JavaScript30/keySequenceDetection/app.js
@@ -1,12 +1,13 @@
 const pressed = [];
 const secretCode = "matilde";
 
-window.addEventListener("keyup", (e) => {
+window.addEventListener("keydown", (e) => {
     pressed.push(e.key);
     pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
     console.log(pressed);
     
-    if (pressed.join("").includes(secretCode)) {
+    if (pressed.join("").toLowerCase().includes(secretCode)) {
         cornify_add();
     }
 });
+
