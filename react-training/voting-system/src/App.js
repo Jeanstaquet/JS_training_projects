53f40931b2b7f395a20810400648f0ee3da20827
Jeanstaquet <jean.staquet8@gmail.com>
2020-02-29 00:55:21 +0100

    Updates

---
+++ react-training/voting-system/src/App.js
@@ -0,0 +1,47 @@
+import React,{Component} from 'react';
+import './App.css';
+
+class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			languages : [
+				{name: "Php", votes: 0},
+				{name: "Python", votes: 0},
+				{name: "Go", votes: 0},
+				{name: "Java", votes: 0}
+			]
+		}
+	}
+
+	vote (i) {
+		let newLanguages = [...this.state.languages];
+		newLanguages[i].votes++;
+		this.setState({languages: newLanguages});
+		
+	}
+
+	render(){
+		return(
+			<>
+				<h1>Vote Your Language!</h1>
+				<div className="languages">
+					{
+						this.state.languages.map((lang, i) => 
+							<div key={i} className="language">
+								<div className="voteCount">
+									{lang.votes}
+								</div>
+								<div className="languageName">
+									{lang.name}
+								</div>
+								<button onClick={this.vote.bind(this, i)}>Click Here</button>
+							</div>
+						)
+					}
+				</div>
+			</>
+		);
+	}
+}
+export default App;
