9db8258b6f917c39c7dbfa549022f7aff09152c8
Jeanstaquet <jean.staquet8@gmail.com>
2020-03-03 11:09:38 +0100

    StateHandler, dynamic style

--- react-training/todo-list/src/App.js
+++ react-training/todo-list/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  state = {
+    changeColor: false
+  };
+
+  changeColorHandling = (e) => {
+    const doeschange = this.state.changeColor;
+    this.setState({changeColor:!doeschange});
+    console.log(this.state.changeColor);
+    e.preventDefault()
+  }
+  render() {
+    const styleBasic= {
+      padding: "8px",
+      margin: "8px"
+    };
+
+    const styleButton= {
+        backgroundColor: "#4CAF50", 
+        border: "none",
+        color: "white",
+        padding: "15px 32px",
+        textAlign: "center",
+        textDecoration: "none",
+        display: "inline-block",
+        fontSize: "16px",
+      };
+
+    if(this.state.changeColor) {
+      styleButton.backgroundColor= "pink";
+    }
+    return (
+      <div className="App">
+        <form style={styleBasic}>
+          <label>
+            Add an item to the list:
+            <input type="text" name="name" id="input"/>
+          </label>
+          <inputs type="submit" value="Submit" style={styleButton} onClick={(e) => this.changeColorHandling(e)}/>
+        </form>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      </div>
+    );
+  }
+};
 
 export default App;
