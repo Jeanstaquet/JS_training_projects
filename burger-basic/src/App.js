2c5bae57815a3669f33bcf0ec6a9aa6874315589
Jeanstaquet <jean.staquet8@gmail.com>
2020-03-29 23:29:29 +0200

    Ajout des ingrédients (js + CSS), Commencement du burger

--- burger-basic/src/App.js
+++ burger-basic/src/App.js
@@ -1,19 +1,19 @@
 import React, {Component} from 'react';
 import Layout from "./components/Layout/Layout";
-
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder"
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Test of the layout Component</p>
+          <BurgerBuilder />
         </Layout>
       </div>
 
     );
   }
-}
+};
 
 
 export default App;
--- burger-basic/src/components/Layout/Layout.js
+++ burger-basic/src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Auxiliary from "../../hoc/Auxiliary";
+import "./Layout.css";
 
 const layout = (props) => (
     <Auxiliary>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={"Content"}>
             {props.children}
         </main>
     </Auxiliary>
