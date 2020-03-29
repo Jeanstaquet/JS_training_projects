d16844e5577df7ae5d1770a42bc451fe571e50dd
Jeanstaquet <jean.staquet8@gmail.com>
2020-03-29 20:02:34 +0200

    Ajout component: Layout, hoc

---
+++ burger-basic/src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Layout from "./components/Layout/Layout";
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p>Test of the layout Component</p>
+        </Layout>
+      </div>
+
+    );
+  }
+}
+
+
+export default App;
