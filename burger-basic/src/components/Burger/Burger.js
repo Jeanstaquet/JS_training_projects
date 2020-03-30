834fc83bd50413c11b78cd686abeb96da14f42e7
Jeanstaquet <jean.staquet8@gmail.com>
2020-03-30 19:06:33 +0200

    Ajout de la variable dynamique pour outputer les ingrédients selon le state

--- burger-basic/src/components/Burger/Burger.js
+++ burger-basic/src/components/Burger/Burger.js
@@ -3,11 +3,16 @@ import "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredients";
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        });
     return (
         <div className={"Burger"}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- burger-basic/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-basic/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,18 @@ import Auxiliary from "../../hoc/Auxiliary"
 import Burger from "../../components/Burger/Burger"
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 2,
+            cheese: 2,
+            meat: 2
+        }
+    };
     render() {
         return (
             <Auxiliary>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Auxiliary>
         );
