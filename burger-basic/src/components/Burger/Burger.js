594cd3a550e7f519fa76730a064704f05fe0c090
Jeanstaquet <jean.staquet8@gmail.com>
2020-03-30 23:27:44 +0200

    Ajout d'un controle, to do: ajout de tout les contrôles dans l'array

--- burger-basic/src/components/Burger/Burger.js
+++ burger-basic/src/components/Burger/Burger.js
@@ -3,12 +3,20 @@ import "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredients";
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }    
+
     return (
         <div className={"Burger"}>
             <BurgerIngredient type="bread-top" />
