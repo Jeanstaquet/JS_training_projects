6e1e1e697933bb09df4396e10c69f377fa330490
Jeanstaquet <jean.staquet8@gmail.com>
2020-04-04 18:08:45 +0200

    finition du projet pour l'instant, next-step: implémenter les requêtes HTTP

--- burger-basic/src/components/UI/Modal/Modal.js
+++ burger-basic/src/components/UI/Modal/Modal.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Modal.css";
 import Auxiliary from "../../../hoc/Auxiliary";
 import Backdrop from "../Backdrop/Backdrop"
 
-const modal = (props) => (
-    <Auxiliary>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div className={"Modal"}
-        style={{
-            transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-            opacity: props.show ? "1": "0"
-        }}>
-            {props.children}
-        </div>
-    </Auxiliary>
-);
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+    render() {
+        return (
+            <Auxiliary>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={"Modal"}
+                style={{
+                    transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+                    opacity: this.props.show ? "1": "0"
+                }}>
+                    {this.props.children}
+                </div>
+            </Auxiliary>
+        )
+    }
+};
 
-export default modal;
+export default Modal;
