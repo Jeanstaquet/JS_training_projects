8c5ab408762a9cad1cc333ee9abb76cc422a6ee6
Jeanstaquet <jean.staquet8@gmail.com>
2020-04-08 22:52:53 +0200

    Installation des packages, to-do: debounce

--- evernote-clone-react/src/index.js
+++ evernote-clone-react/src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require("firebase")
+require("firebase/firestore")
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBajW2gSO7y9pSaFmPFVFqgbq1BKu4-gas",
+  authDomain: "evernote-clone-a2db3.firebaseapp.com",
+  databaseURL: "https://evernote-clone-a2db3.firebaseio.com",
+  projectId: "evernote-clone-a2db3",
+  storageBucket: "evernote-clone-a2db3.appspot.com",
+  messagingSenderId: "101179934358",
+  appId: "1:101179934358:web:fa09e81a3383ef4d87a4b7",
+  measurementId: "G-W6E8BFWKM8"
+});
+firebase.analytics();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
